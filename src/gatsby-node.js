0c1e579ee7791578af4d148b084518058b752d96
Art Tosborvorn <art.tosborvorn@gmail.com>
2021-02-09 15:03:15 +0700

    z

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -87,7 +87,7 @@ exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
           languages: pageLanguages,
           messages,
           routed,
-          originalPath: page.context.originalPath,
+          originalPath: tmpPath,
           redirect,
           defaultLanguage,
         },
